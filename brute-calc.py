4d16311b325f91e13a887e5927b0eb740f03049b
Tushar maharana <54012021+tusharhero@users.noreply.github.com>
2021-06-06 14:25:31 +0530

    added more time units
    earlier it was outputting time remaining in seconds but now it shows it in seconds, minutes, hours, days, months and years.

--- brute-calc.py
+++ brute-calc.py
@@ -1,4 +1,4 @@
-print("brute-calc v0.01")
+print("brute-calc v0.02")
 #made bt Tushar Maharana
 
 #asking for the length of the keyword and converting it to integer
@@ -17,11 +17,30 @@ speed_brute = input("enter the speed of Bruteforce in k/s:")
 speed_brute = float(speed_brute)
 
 #calculating the time required
-tr = n_key/speed_brute
+
+#seconds
+tr_s = n_key/speed_brute
+
+#minutes
+tr_min = tr_s/60
+
+#hours
+tr_hr = tr_min/60
+
+#days
+tr_d = tr_hr/24
+
+#months
+tr_m = tr_d/30
+
+#years
+tr_y = tr_d/365
+
+
 
 #printing everthing
 print("length of the keyword:",length,"characters")
 print("number of characters in character set:",n_chars_in_charset)
 print("number of keywords:",n_key,"keywords")
 print("the speed of Bruteforce:",speed_brute,"k/s")
-print("time required:",tr,"s")
+print("time required:",tr_s,"s ,",tr_min,"min ,",tr_hr,"hr ,",tr_d,"days ,",tr_m,"months ,",tr_y,"years")
